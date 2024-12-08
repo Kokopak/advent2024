6ff84aa975daa326dc367451d4053dc879e3a9a6
Corentin Chataignon <corentin.chataignon44@gmail.com>
2024-12-09 00:33:00 +0100

    chore(day04): refacto day04

--- day04/day04.py
+++ day04/day04.py
@@ -1,63 +1,55 @@
 with open("input.txt") as f:
-    words = [list(l.strip()) for l in f]
+    letters = {}
+    for ir, r in enumerate(f.readlines()):
+        for ic, c in enumerate(r.strip()):
+            letters[(ir, ic)] = c
 
-rows, cols = (len(words), len(words[0]))
+    ROWS = ir + 1
+    COLS = ic + 1
+
+
+def check_p1(r, c, dr, dc, word):
+    return (
+        letters.get((r, c)) == word[0]
+        and letters.get((r + dr, c + dc)) == word[1]
+        and letters.get((r + 2 * dr, c + 2 * dc)) == word[2]
+        and letters.get((r + 3 * dr, c + 3 * dc)) == word[3]
+    )
+
 
 counter = 0
 counter2 = 0
 
-for i in range(rows * cols):
-    r = i // cols
-    c = (i - cols) % cols
+patterns = {"X": ["X", "M", "A", "S"], "S": ["S", "A", "M", "X"]}
 
-    # part 1
-    poss = ["", "", "", ""]
 
-    if c + 3 < cols:
-        poss[1] = words[r][c] + words[r][c + 1] + words[r][c + 2] + words[r][c + 3]
+for r, c in letters:
+    if letters[r, c] in patterns:
+        word = patterns[letters[r, c]]
 
-    if r + 3 < rows:
-        poss[0] = words[r][c] + words[r + 1][c] + words[r + 2][c] + words[r + 3][c]
-
-        if c + 3 < cols:
-            poss[2] = (
-                words[r][c]
-                + words[r + 1][c + 1]
-                + words[r + 2][c + 2]
-                + words[r + 3][c + 3]
-            )
-
-        if c - 3 >= 0:
-            poss[3] = (
-                words[r][c]
-                + words[r + 1][c - 1]
-                + words[r + 2][c - 2]
-                + words[r + 3][c - 3]
-            )
-
-    for p in poss:
-        if p == "XMAS" or p[::-1] == "XMAS":
+        if c + 3 < COLS and check_p1(r, c, 0, 1, word):
             counter += 1
 
-    # part 2
-    poss2 = ["", ""]
+        if r + 3 < ROWS:
+            if check_p1(r, c, 1, 0, word):
+                counter += 1
 
-    if words[r][c] == "A":
-        if r - 1 >= 0 and c - 1 >= 0 and r + 1 < rows and c + 1 < cols:
-            poss2[0] = words[r - 1][c - 1] + words[r][c] + words[r + 1][c + 1]
-            poss2[1] = words[r - 1][c + 1] + words[r][c] + words[r + 1][c - 1]
+            if c + 3 < COLS:
+                if check_p1(r, c, 1, 1, word):
+                    counter += 1
 
-        if poss2[0] == "MAS" and poss2[1] == "MAS":
-            counter2 += 1
-
-        if poss2[0] == "SAM" and poss2[1] == "SAM":
-            counter2 += 1
-
-        if poss2[0] == "SAM" and poss2[1] == "MAS":
-            counter2 += 1
-
-        if poss2[0] == "MAS" and poss2[1] == "SAM":
-            counter2 += 1
+            if c - 3 >= 0:
+                if check_p1(r, c, 1, -1, word):
+                    counter += 1
+    elif letters[r, c] == "A":
+        if r - 1 >= 0 and c - 1 >= 0 and r + 1 < ROWS and c + 1 < COLS:
+            if (letters[r - 1, c - 1] == "M" and letters[r + 1, c + 1] == "S") or (
+                letters[r - 1, c - 1] == "S" and letters[r + 1, c + 1] == "M"
+            ):
+                if (letters[r - 1, c + 1] == "M" and letters[r + 1, c - 1] == "S") or (
+                    letters[r - 1, c + 1] == "S" and letters[r + 1, c - 1] == "M"
+                ):
+                    counter2 += 1
 
 print(counter)
 print(counter2)
